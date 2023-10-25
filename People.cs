18243eae38e2d570e9cfc30f9b0c285d053fa1d7
Rozmyslova <108892197+Rozmyslova@users.noreply.github.com>
2023-10-25 04:43:54 +0300

    Added first version

---
+++ People.cs
@@ -0,0 +1,200 @@
+﻿internal class People
+{
+    private int id;
+    private string personName;
+    private string gender;
+    private string dateOfBitrh;
+    private string mother;
+    private string father;
+    private string maritalStatus;
+    private string partner;
+
+    static List<People> membersOfFamily()
+    {
+
+        People person1 = new People
+        {
+            id = 1,
+            personName = "Романов Николай Аександрович",
+            gender = "Мужской",
+            dateOfBitrh = "18.5.1868",
+            mother = "Романова Мария Федоровна",
+            father = "Романов Александр Александрович",
+            maritalStatus = "Женат",
+            partner = "Романова Александра Федоровна"
+        };
+
+        People person2 = new People
+        {
+            id = 2,
+            personName = "Романова Александра Федоровна",
+            gender = "Женский",
+            dateOfBitrh = "6.6.1872",
+            mother = "Гессенская Алиса",
+            father = "Гессенский Людвиг",
+            maritalStatus = "Замужем",
+            partner = "Романов Николай Аександрович"
+        };
+
+        People person3 = new People
+        {
+            id = 3,
+            personName = "Романова Ольга Николаевна",
+            gender = "Женский",
+            dateOfBitrh = "3.11.1895",
+            mother = "Романова Александра Федоровна",
+            father = "Романов Николай Аександрович",
+            maritalStatus = "Не замужем",
+            partner = "Нет"
+        };
+
+        People person4 = new People
+        {
+            id = 4,
+            personName = "Романова Татьяна Николаевна",
+            gender = "Женский",
+            dateOfBitrh = "10.6.1897",
+            mother = "Романова Александра Федоровна",
+            father = "Романов Николай Аександрович",
+            maritalStatus = "Не замужем",
+            partner = "Нет"
+        };
+
+        People person5 = new People
+        {
+            id = 5,
+            personName = "Романова Мария Николаевна",
+            gender = "Женский",
+            dateOfBitrh = "26.6.1899",
+            mother = "Романова Александра Федоровна",
+            father = "Романов Николай Аександрович",
+            maritalStatus = "Не замужем",
+            partner = "Нет"
+        };
+
+        People person6 = new People
+        {
+            id = 6,
+            personName = "Романова Анастасия Николаевна",
+            gender = "Женский",
+            dateOfBitrh = "18.6.1901",
+            mother = "Романова Александра Федоровна",
+            father = "Романов Николай Аександрович",
+            maritalStatus = "Не замужем",
+            partner = "Нет"
+        };
+
+        People person7 = new People
+        {
+            id = 7,
+            personName = "Романов Алексей Николаевич",
+            gender = "Мужской",
+            dateOfBitrh = "12.8.1904",
+            mother = "Романова Александра Федоровна",
+            father = "Романов Николай Аександрович",
+            maritalStatus = "Не женат",
+            partner = "Нет"
+        };
+
+        People person8 = new People
+        {
+            id = 8,
+            personName = "Романов Александр Александрович",
+            gender = "Мужской",
+            dateOfBitrh = "10.3.1845",
+            mother = "Романова Мария Александровна",
+            father = "Романов Александр Николаевич",
+            maritalStatus = "Женат",
+            partner = "Романова Мария Федоровна"
+        };
+
+        People person9 = new People
+        {
+            id = 9,
+            personName = "Романова Мария Федоровна",
+            gender = "Женский",
+            dateOfBitrh = "26.11.1847",
+            mother = "Глюксборг Луиза",
+            father = "Глюксборг Кристиан",
+            maritalStatus = "Замужем",
+            partner = "Романов Александр Александрович"
+        };
+
+        People person10 = new People
+        {
+            id = 10,
+            personName = "Гессенский Людвиг",
+            gender = "Мужской",
+            dateOfBitrh = "12.9.1837",
+            mother = "Прусская Елизавета",
+            father = "Гессенский Карл",
+            maritalStatus = "Женат",
+            partner = "Гессенская Алиса"
+        };
+
+        People person11 = new People
+        {
+            id = 11,
+            personName = "Гессенская Алиса",
+            gender = "Женский",
+            dateOfBitrh = "25.4.1843",
+            mother = "Александрина Виктория",
+            father = "Альберт Саксен-Кобург-Готского",
+            maritalStatus = "Замужем",
+            partner = "Гессенский Людвиг"
+        };
+
+        People person12 = new People
+        {
+            id = 12,
+            personName = "Романов Александр Аександрович",
+            gender = "Мужской",
+            dateOfBitrh = "26.5.1869",
+            mother = "Романова Мария Федоровна",
+            father = "Романов Александр Александрович",
+            maritalStatus = "Не женат",
+            partner = "Нет"
+        };
+
+        People person13 = new People
+        {
+            id = 13,
+            personName = "Романов Георгий Аександрович",
+            gender = "Мужской",
+            dateOfBitrh = "27.4.1871",
+            mother = "Романова Мария Федоровна",
+            father = "Романов Александр Александрович",
+            maritalStatus = "Не женат",
+            partner = "Нет"
+        };
+
+        People person14 = new People
+        {
+            id = 14,
+            personName = "Романова Ксения Аександровна",
+            gender = "Женский",
+            dateOfBitrh = "25.3.1875",
+            mother = "Романова Мария Федоровна",
+            father = "Романов Александр Александрович",
+            maritalStatus = "Замужем",
+            partner = "Романов Александр Михайлович"
+        };
+
+        List<People> familyMembers = new List<People>();
+        familyMembers.Add(person1);
+        familyMembers.Add(person2);
+        familyMembers.Add(person3);
+        familyMembers.Add(person4);
+        familyMembers.Add(person5);
+        familyMembers.Add(person6);
+        familyMembers.Add(person7);
+        familyMembers.Add(person8);
+        familyMembers.Add(person9);
+        familyMembers.Add(person10);
+        familyMembers.Add(person11);
+        familyMembers.Add(person12);
+        familyMembers.Add(person13);
+        familyMembers.Add(person14);
+        return familyMembers;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Nest;
+using System;
+using System.Collections.Generic;
+
+
+public class FamilyTree
+{
+    private object? personName;
+    private object? gender;
+    private object? dateOfBitrh;
+    private object? father;
+    private object? mother;
+    private object? maritalStatus;
+
+    public static void Main(string[] args)
+    { 
+             
+        /*void info(FamilyTree familyTree)
+        {
+            int id;
+            string personName;
+            string gender;
+            string dateOfBitrh;
+            string mother;
+            string father;
+            string maritalStatus;
+            string partner;
+
+            Console.WriteLine();
+            Console.WriteLine("ФИО: {0}", familyTree.personName);
+            Console.WriteLine("Пол: {0}", familyTree.gender);
+            Console.WriteLine("Дата рождения: {0}", familyTree.dateOfBitrh);
+            Console.WriteLine("Мать: {0}", familyTree.mother);
+            Console.WriteLine("Отец: {0}", familyTree.father);
+            Console.WriteLine("Семейное положение: {0}", familyTree.maritalStatus);
+        }*/
+    
+        List<People> tree = new(membersOfFamily());
+
+        People user = new People();
+        Console.WriteLine("1 - Романов Николай Аександрович");
+        Console.WriteLine("2 - Романова Александра Федоровна");
+        Console.WriteLine("3 - Романова Ольга Николаевна");
+        Console.WriteLine("4 - Романова Татьяна Николаевна");
+        Console.WriteLine("5 - Романова Мария Николаевна");
+        Console.WriteLine("6 - Романова Анастасия Николаевна");
+        Console.WriteLine("7 - Романов Алексей Николаевич");
+        Console.WriteLine("8 - Романов Александр Александрович");
+        Console.WriteLine("9 - Романова Мария Федоровна");
+        Console.WriteLine("10 - Гессенский Людвиг");
+        Console.WriteLine("11 - Гессенская Алиса");
+        Console.WriteLine("12 - Романов Александр Аександрович");
+        Console.WriteLine("13 - Романов Георгий Аександрович");
+        Console.WriteLine("14 - Романова Ксения Аександровна");
+        Console.Write("Укажите ID человека из генеалогического древа (1-14): ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int i = 0; i < tree.Count; i++)
+        {
+            if (id == tree[i].id)
+            {
+                tree[i].info();
+                user = tree[i];
+                if (tree[i].partner.Equals("Не замужем") || (user.partner.Equals("Не женат")))
+                {
+                    Console.WriteLine("Нет спутника жизни");
+                }
+                else
+                {
+                    if (tree[i].gender.Equals("Женский"))
+                    {
+                        Console.WriteLine("Муж : {0}", tree[i].partner);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Жена : {0}", tree[i].partner);
+                    }
+                }
+            }
+        }
+        Console.WriteLine();
+
+        List<People> children = new List<People>();
+        for (int i = 0; i < tree.Count; i++)
+        {
+            if (user.personName != tree[i].personName && user.dateOfBitrh != tree[i].dateOfBitrh)
+            {
+                if (user.mother.Equals(tree[i].mother) || user.father.Equals(tree[i].father))
+                {
+                    if (tree[i].gender.Equals("Женский"))
+                    {
+                        Console.WriteLine("Сестра: {0}", tree[i].personName);
+                    }
+                    if (!tree[i].gender.Equals("Мужской"))
+                    {
+                    }
+                    else
+                    {
+                        Console.WriteLine("Брат: {0}", tree[i].personName);
+                    }
+                }
+                if (user.partner.Equals(tree[i].personName))
+                {
+                    if (user.gender.Equals("Мужской"))
+                    {
+                        Console.WriteLine("Теща: {0}", tree[i].mother);
+                        Console.WriteLine("Тесть: {0}", tree[i].father);
+                    }
+                    if (user.gender.Equals("Женский"))
+                    {
+                        Console.WriteLine("Свекровь: {0}", tree[i].mother);
+                        Console.WriteLine("Свекор: {0}", tree[i].father);
+                    }
+                }
+                if (!user.personName.Equals(tree[i].father) && !user.personName.Equals(tree[i].mother))
+                {
+                    continue;
+                }
+                children.Add(tree[i]);
+            }
+        }
+
+        
+
+    }
+
+    private static IEnumerable<People> membersOfFamily()
+    {
+        throw new NotImplementedException();
+    }
+}
+
